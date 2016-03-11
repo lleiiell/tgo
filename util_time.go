cef4c2bfc7f6ee657bc0a88897d8131f4bdd4673
tonyjt <tonyliujiangtao@gmail.com>
2016-03-11 18:49:03 +0800

    1.0.2
    添加UtilTimeIsToday

--- util_time.go
+++ util_time.go
@@ -10,3 +10,10 @@ func UtilTimeGetDate(t time.Time) time.Time {
 
 	return time.Date(year, month, day, 0, 0, 0, 0, t.Location())
 }
+
+func UtilTimeIsToday(t time.Time) bool {
+	if t.Format("20060102") == time.Now().Format("20060102") {
+		return true
+	}
+	return false
+}
